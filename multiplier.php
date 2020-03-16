65c56666150bbe0eb2f798cd33f7789be6f021e1
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-16 14:52:40 +0100

    level 2 opdr 1

--- multiplier.php
+++ multiplier.php
@@ -1,5 +1,5 @@
 <?php
-echo 'we gaan 2 getallen met elkaar vermedigvuldigen'. ' ';
+echo 'we gaan 2 getallen met elkaar vermedigvuldigen';
 $getal1 = readline('geef je eerste getal op');
 $getal2 = readline('geef je tweede getal op');
 echo 'de uitkomst is ' . $getal1 * $getal2;
